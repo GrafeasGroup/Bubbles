388822367c16f47a9caa6aae48809ca72858ed45
Joe Kaufeld <joe@kenzie.academy>
2020-08-13 15:10:51 -0400

    it would help if I added that as a decorator
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- bubbles/commands/update_and_restart.py
+++ bubbles/commands/update_and_restart.py
@@ -6,10 +6,12 @@ from bubbles.helpers import fire_and_forget
 
 
 def update(data) -> None:
+
+    @fire_and_forget
     def trigger_update():
-        subprocess.run(os.path.join(os.getcwd(), "update.py"))
+        subprocess.run(os.path.join(os.getcwd(), "update.py"), shell=True)
 
     print(os.getcwd())
-    fire_and_forget(trigger_update)
+    trigger_update()
 
 PluginManager.register_plugin(update, r"update$", help="!update - pull changes from github and restart!")
