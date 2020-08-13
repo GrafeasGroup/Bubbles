ab1199a56360cc48281628959ae267f012ed1264
Joe Kaufeld <joe@kenzie.academy>
2020-08-13 15:45:07 -0400

    move to different function
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- bubbles/commands/update_and_restart.py
+++ bubbles/commands/update_and_restart.py
@@ -4,20 +4,27 @@ import subprocess
 from bubbles.config import PluginManager, client
 from bubbles.helpers import fire_and_forget
 
-
-def update(data) -> None:
-    client.chat_postMessage(
-        channel=data.get("channel"),
-        text="Getting ready to try to update. About to call `{}`".format(
-            [os.path.join(os.getcwd(), ".venv", "bin", "python"), "update.py"]
-        ),
-        as_user=True,
-    )
+@fire_and_forget
+def update_shizz():
     subprocess.Popen(
         [
             os.path.join(os.getcwd(), ".venv", "bin", "python"),
             os.path.join(os.getcwd(), "update.py")
         ]
     )
+
+
+def update(data) -> None:
+    client.chat_postMessage(
+        channel=data.get("channel"),
+        text="Getting ready to try to update. About to call `{}`".format(
+            [
+                os.path.join(os.getcwd(), ".venv", "bin", "python"),
+                os.path.join(os.getcwd(), "update.py")
+            ]
+        ),
+        as_user=True,
+    )
+    update_shizz()
 
 PluginManager.register_plugin(update, r"update$", help="!update - pull changes from github and restart!")
