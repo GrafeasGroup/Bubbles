c9f70c12ff72e7a5634164dc5cc01bf0b461c49a
Joe Kaufeld <joe@kenzie.academy>
2020-08-13 15:27:00 -0400

    debugging
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- bubbles/commands/update_and_restart.py
+++ bubbles/commands/update_and_restart.py
@@ -1,11 +1,18 @@
 import os
 import subprocess
 
-from bubbles.config import PluginManager
+from bubbles.config import PluginManager, client
 from bubbles.helpers import fire_and_forget
 
 
 def update(data) -> None:
+    client.chat_postMessage(
+        channel=data.get("channel"),
+        text="Getting ready to try to update. About to call `{}`".format(
+            [os.path.join(os.getcwd(), ".venv", "bin", "python"), "update.py"]
+        ),
+        as_user=True,
+    )
     subprocess.call([os.path.join(os.getcwd(), ".venv", "bin", "python"), "update.py"])
 
 PluginManager.register_plugin(update, r"update$", help="!update - pull changes from github and restart!")
