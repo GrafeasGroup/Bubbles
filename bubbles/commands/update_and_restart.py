9ee1f2f981a9150b0ea206e88775afb51a1f71b1
Joe Kaufeld <joe@kenzie.academy>
2020-08-13 15:22:51 -0400

    let's remove threading'
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- bubbles/commands/update_and_restart.py
+++ bubbles/commands/update_and_restart.py
@@ -6,12 +6,6 @@ from bubbles.helpers import fire_and_forget
 
 
 def update(data) -> None:
-
-    @fire_and_forget
-    def trigger_update():
-        subprocess.call([os.path.join(os.getcwd(), "python"), "update.py"])
-
-    print(os.getcwd())
-    trigger_update()
+    subprocess.call([os.path.join(os.getcwd(), "python"), "update.py"])
 
 PluginManager.register_plugin(update, r"update$", help="!update - pull changes from github and restart!")
