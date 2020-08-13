8c46ae0a6df6dd70dbc92c171207d480b9fd4dce
Joe Kaufeld <joe@kenzie.academy>
2020-08-13 15:23:50 -0400

    adjust path
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- bubbles/commands/update_and_restart.py
+++ bubbles/commands/update_and_restart.py
@@ -6,6 +6,6 @@ from bubbles.helpers import fire_and_forget
 
 
 def update(data) -> None:
-    subprocess.call([os.path.join(os.getcwd(), "python"), "update.py"])
+    subprocess.call([os.path.join(os.getcwd(), ".venv", "bin", "python"), "update.py"])
 
 PluginManager.register_plugin(update, r"update$", help="!update - pull changes from github and restart!")
