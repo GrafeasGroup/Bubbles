bb8dec01e32f3581b23b7504f5f3ab4e28fc49d8
Joe Kaufeld <joe@kenzie.academy>
2020-10-04 21:10:09 -0400

    clean up periodic commands
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- bubbles/commands/periodic/welcome_ping.py
+++ bubbles/commands/periodic/welcome_ping.py
@@ -1,9 +1,9 @@
 import datetime
 
-from bubbles.config import client, users_list, rooms_list, mods_array, reddit
+from bubbles.config import client, users_list, rooms_list, mods_array, DEFAULT_CHANNEL
 
 
-def periodic_ping_callback() -> None:
+def welcome_ping_callback() -> None:
     timestamp_needed_end = datetime.datetime.now() - datetime.timedelta(days=7)
     timestamp_needed_start = datetime.datetime.now() - datetime.timedelta(hours=4)
     response = client.conversations_history(
@@ -52,8 +52,8 @@ def periodic_ping_callback() -> None:
                 channel=rooms_list["new_volunteers_meta"],
                 link_names=1,
                 text="<@"
-                + id_mod_to_ping
-                + "> there are unwelcomed users after 4 hours. Please fix that as soon as possible.",
+                     + id_mod_to_ping
+                     + "> there are unwelcomed users after 4 hours. Please fix that as soon as possible.",
                 as_user=True,
             )
             client.chat_postMessage(
@@ -63,49 +63,10 @@ def periodic_ping_callback() -> None:
                 as_user=True,
             )
 
-
-#    else:
-#        response = client.chat_postMessage(
-#            channel=DEFAULT_CHANNEL,
-#            text="All users have been welcomed. Good.",
-#            as_user=True,
-#        )
-#    print("Trigger time:" + str(datetime.datetime.now()))
-
-
-def banbot_check_callback() -> None:
-    subreddits = (
-        reddit.subreddit("TranscribersOfReddit")
-        .wiki["subreddits"]
-        .content_md.splitlines()
-    )
-    saferbot_list = list()
-    misandrybot_list = list()
-
-    for sub in subreddits:
-        try:
-            mods = [mod.name.lower() for mod in reddit.subreddit(sub).moderator()]
-            if "saferbot" in mods:
-                saferbot_list.append(sub)
-            if "misandrybot" in mods:
-                misandrybot_list.append(sub)
-        except:
-            print(f"banbot_check: FAILED TO GET {sub}")
-
-    message = (
-        ":rotating_light: :radioactive_sign:{0}:radioactive_sign:"
-        " detected in the following subreddits: {1} :rotating_light:"
-    )
-
-    if len(saferbot_list) > 0:
-        client.chat_postMessage(
-            channel=rooms_list["general"],
-            text=message.format("SaferBot", ", ".join(saferbot_list)),
-            as_user=True,
-        )
-    if len(misandrybot_list) > 0:
-        client.chat_postMessage(
-            channel=rooms_list["general"],
-            text=message.format("MisandryBot", ", ".join(misandrybot_list)),
-            as_user=True,
-        )
+    # else:
+    #     response = client.chat_postMessage(
+    #         channel=DEFAULT_CHANNEL,
+    #         text="All users have been welcomed!",
+    #         as_user=True,
+    #     )
+    # print("Trigger time:" + str(datetime.datetime.now()))
--- bubblesRTM.py
+++ bubblesRTM.py
@@ -4,10 +4,11 @@ import traceback
 import timeloop
 from slack import RTMClient
 
+from bubbles.commands.periodic.banbot_check import banbot_check_callback
+from bubbles.commands.periodic.welcome_ping import welcome_ping_callback
 from bubbles.config import client, rtm_client
 from bubbles.hello import hello_callback
 from bubbles.message import process_message
-from bubbles.periodic_commands import periodic_ping_callback, banbot_check_callback
 from bubbles.reaction_added import reaction_added_callback
 
 tl = timeloop.Timeloop()
@@ -38,7 +39,7 @@ def func(**payload):
 
 @tl.job(interval=datetime.timedelta(seconds=3000))
 def periodic_ping():
-    periodic_ping_callback()
+    welcome_ping_callback()
 
 
 @tl.job(interval=datetime.timedelta(hours=12))
