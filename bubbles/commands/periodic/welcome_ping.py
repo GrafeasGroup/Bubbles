2317b6de10f075ac49cc2285482d9ba90e8165d1
Joe Kaufeld <joe@kenzie.academy>
2020-11-26 00:56:34 -0500

    update welcome ping to generate permalinks

--- bubbles/commands/periodic/welcome_ping.py
+++ bubbles/commands/periodic/welcome_ping.py
@@ -1,6 +1,18 @@
 import datetime
 
-from bubbles.config import client, users_list, rooms_list, mods_array, DEFAULT_CHANNEL
+from bubbles.config import client, users_list, rooms_list, mods_array
+
+VOLUNTEER_CHANNEL = "new_volunteers"
+META_CHANNEL = "new_volunteers_meta"
+IN_PROGRESS_CHANNEL = "new_volunteers_pings_in_progress"
+
+
+def get_username_and_permalink(message):
+    username = message["text"].split(" ")[0].split("|")[1][:-1]
+    permalink = client.chat_getPermalink(
+        channel=rooms_list[VOLUNTEER_CHANNEL], message_ts=message['ts']
+    ).data.get('permalink')
+    return username, permalink
 
 
 def welcome_ping_callback() -> None:
@@ -8,12 +20,12 @@ def welcome_ping_callback() -> None:
     timestamp_needed_start_cry = datetime.datetime.now() - datetime.timedelta(hours=4)
 
     response = client.conversations_history(
-        channel=rooms_list["new_volunteers"],
+        channel=rooms_list[VOLUNTEER_CHANNEL],
         oldest=timestamp_needed_end_cry.timestamp(),
         latest=timestamp_needed_start_cry.timestamp(),
     )  # ID for #bottest
     cry = False
-    list_users_to_welcome = []
+    users_to_welcome = {}
     for message in response["messages"]:
         # print(message["text"])
         if "reactions" not in message.keys():
@@ -27,42 +39,55 @@ def welcome_ping_callback() -> None:
                     pass
         if no_valable_reactions:
             cry = True
-            name_user_to_welcome = message["text"].split(" ")[0]
-            name_user_to_welcome = name_user_to_welcome.split("|")[1]
-            name_user_to_welcome = name_user_to_welcome[:-1]
-            list_users_to_welcome.append(name_user_to_welcome)
+            try:
+                username, permalink = get_username_and_permalink(message)
+            except IndexError:
+                # This is a message that didn't come from Kierra and isn't something we
+                # can process. Ignore it and move onto the next message.
+                continue
+            users_to_welcome[username] = permalink
     if cry:
         hour = datetime.datetime.now().hour
         person_to_ping = mods_array[hour]
+
+        # First figure out who we're going to ping, then follow up with the list of
+        # users.4
         if person_to_ping is None:
             client.chat_postMessage(
-                channel=rooms_list["new_volunteers_meta"],
+                channel=rooms_list[META_CHANNEL],
                 link_names=1,
-                text="There are unwelcomed users, but there is no mod to ping in the schedule. Please welcome the new users as soon as possible.",
-                as_user=True,
-            )
-            client.chat_postMessage(
-                channel=rooms_list["new_volunteers_meta"],
-                link_names=1,
-                text="List of unwelcomed users: " + str(list_users_to_welcome),
+                text=(
+                    "There are unwelcomed users, but there is no mod to ping in the"
+                    " schedule. Please welcome the new users as soon as possible."
+                ),
                 as_user=True,
             )
         else:
             id_mod_to_ping = users_list[person_to_ping]
             client.chat_postMessage(
-                channel=rooms_list["new_volunteers_meta"],
+                channel=rooms_list[META_CHANNEL],
                 link_names=1,
-                text="<@"
-                + id_mod_to_ping
-                + "> there are unwelcomed users after 4 hours. Please fix that as soon as possible.",
+                text=(
+                    f"<@{id_mod_to_ping}> there are unwelcomed users after 4 hours."
+                    f" Please fix that as soon as possible."
+                ),
                 as_user=True,
             )
-            client.chat_postMessage(
-                channel=rooms_list["new_volunteers_meta"],
-                link_names=1,
-                text="List of unwelcomed users: " + str(list_users_to_welcome),
-                as_user=True,
-            )
+
+        # <{url}|u/{username}>
+        client.chat_postMessage(
+            channel=rooms_list[META_CHANNEL],
+            link_names=1,
+            text="List of unwelcomed users: " + ", ".join(
+                [
+                    f"<{users_to_welcome[username]}|{username}>"
+                    for username in users_to_welcome
+                ]
+            ),
+            unfurl_links=False,
+            unfurl_media=False,
+            as_user=True,
+        )
 
 
 #    else:
@@ -82,7 +107,7 @@ def periodic_ping_in_progress_callback() -> None:
         hours=24
     )
     response_watchping = client.conversations_history(
-        channel=rooms_list["new_volunteers"],
+        channel=rooms_list[VOLUNTEER_CHANNEL],
         oldest=timestamp_needed_end_watchping.timestamp(),
         latest=timestamp_needed_start_watchping.timestamp(),
     )  # ID for #bottest
@@ -93,58 +118,63 @@ def periodic_ping_in_progress_callback() -> None:
     #        text="Heartbeat message.",
     #        as_user=True,
     #    )
-    list_users_to_check_out = []
+    users_to_check = {}
     for message in response_watchping["messages"]:
         # print(message["text"])
         only_watch = False
         if "reactions" not in message.keys():
-            pass  # No reactions -> already handeled by cry
+            continue  # No reactions -> already handled by cry
         for reaction in message["reactions"]:
-            if reaction["name"] in ["watch", "email", "exclamation", "heavy_exclamation_mark"]:
+            if reaction["name"] in [
+                "watch", "email", "exclamation", "heavy_exclamation_mark"
+            ]:
                 only_watch = True
             if reaction["name"] in ["banhammer"]:
                 only_watch = False
                 break
         if only_watch:
             watchping = True
-            name_user_to_check_out = message["text"].split(" ")[0]
-            name_user_to_check_out = name_user_to_check_out.split("|")[1]
-            name_user_to_check_out = name_user_to_check_out[:-1]
-            list_users_to_check_out.append(name_user_to_check_out)
-
+            username, permalink = get_username_and_permalink(message)
+            users_to_check[username] = permalink
     if watchping:
         hour = datetime.datetime.now().hour
         person_to_ping = mods_array[hour]
         if person_to_ping is None:
             client.chat_postMessage(
-                channel=rooms_list["new_volunteers_pings_in_progress"],
+                channel=rooms_list[IN_PROGRESS_CHANNEL],
                 link_names=1,
-                text="There are users claimed with a :watch:, a :email: or a :exclamation:. Please check them out when their time has come.",
-                as_user=True,
-            )
-            client.chat_postMessage(
-                channel=rooms_list["new_volunteers_pings_in_progress"],
-                link_names=1,
-                text="List of users to check out: " + str(list_users_to_check_out),
+                text=(
+                    "There are users claimed with a :watch:, a :email: or a"
+                    " :exclamation:. Please check them out when their time has come."
+                ),
                 as_user=True,
             )
         else:
             id_mod_to_ping = users_list[person_to_ping]
             client.chat_postMessage(
-                channel=rooms_list["new_volunteers_pings_in_progress"],
+                channel=rooms_list[IN_PROGRESS_CHANNEL],
                 link_names=1,
-                text="<@"
-                + id_mod_to_ping
-                + "> there are users claimed with a :watch:, a :email: or a :exclamation:. Please check them out when their time has come.",
-                as_user=True,
-            )
-            client.chat_postMessage(
-                channel=rooms_list["new_volunteers_pings_in_progress"],
-                link_names=1,
-                text="List of users to check out: " + str(list_users_to_check_out),
+                text=(
+                    f"<@{id_mod_to_ping}> there are users claimed with a :watch:, a"
+                    f" :email: or a :exclamation:. Please check them out when their time"
+                    f" has come."
+                ),
                 as_user=True,
             )
 
+        client.chat_postMessage(
+            channel=rooms_list[IN_PROGRESS_CHANNEL],
+            link_names=1,
+            text="List of users to check out: " + ", ".join(
+                [
+                    f"<{users_to_check[username]}|{username}>"
+                    for username in users_to_check
+                ]
+            ),
+            unfurl_links=False,
+            unfurl_media=False,
+            as_user=True,
+        )
 
 #    else:
 #        response = client.chat_postMessage(
