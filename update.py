e48a6d8246fd888c744a4cdd8e8abedb80401322
Joe Kaufeld <joe@kenzie.academy>
2021-01-17 18:39:14 -0500

    add update recovery system

--- update.py
+++ update.py
@@ -1,31 +1,46 @@
 # This script is supposed to be called automatically by Bubbles.
 # Requires setting up sudoers access per https://unix.stackexchange.com/a/497011
+import os
 import subprocess
-import sys
 import traceback
 
-from bubbles.config import client, DEFAULT_CHANNEL, USERNAME
+from bubbles.config import app, DEFAULT_CHANNEL, USERNAME
 
 git_response = (
     subprocess.check_output(["git", "pull", "origin", "master"]).decode().strip()
 )
 
-client.chat_postMessage(
+app.client.chat_postMessage(
     channel=DEFAULT_CHANNEL, text=git_response, as_user=True,
 )
-client.chat_postMessage(
+app.client.chat_postMessage(
     channel=DEFAULT_CHANNEL, text="Restarting service!", as_user=True,
 )
 
 try:
+    subprocess.check_call(
+        [
+            os.path.join(os.getcwd(), ".venv", "bin", "python"),
+            os.path.join(os.getcwd(), "bubblesRTM.py") + " --startup-check",
+        ]
+    )
+
     # if this command succeeds, the process dies here
     systemctl_response = subprocess.check_output(
         ["sudo", "systemctl", "restart", USERNAME]
     )
 except subprocess.CalledProcessError as e:
-    client.chat_postMessage(
+    app.client.chat_postMessage(
         channel=DEFAULT_CHANNEL,
         text=f"Update failed, could not restart: \n```\n{traceback.format_exc()}```",
         as_user=True,
     )
-    sys.exit(1)
+    git_response = subprocess.check_output(["git", "reset", "--hard"]).decode().strip()
+    app.client.chat_postMessage(
+        channel=DEFAULT_CHANNEL,
+        text=f"Rolling back to previous state:\n```\n{git_response}```",
+        as_user=True,
+    )
+    systemctl_response = subprocess.check_output(
+        ["sudo", "systemctl", "restart", USERNAME]
+    )
