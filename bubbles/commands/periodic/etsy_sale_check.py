54c84605b1fe74626bed3f93958822445678c7a6
Joe Kaufeld <joe@kenzie.academy>
2021-05-18 19:22:30 -0400

    update recent sale callback and add country list

--- bubbles/commands/periodic/etsy_sale_check.py
+++ bubbles/commands/periodic/etsy_sale_check.py
@@ -7,23 +7,52 @@ from etsy2.oauth import EtsyOAuthHelper
 
 from bubbles.config import app, etsy, rooms_list
 
-KEY = os.environ.get('etsy_key')
-SECRET = os.environ.get('etsy_secret')
-SHOP_ID = os.environ.get('etsy_shop_id')
-OAUTH_TOKEN = os.environ.get('etsy_oauth_token')
-OAUTH_TOKEN_SECRET = os.environ.get('etsy_oauth_token_secret')
+KEY = os.environ.get("etsy_key")
+SECRET = os.environ.get("etsy_secret")
+SHOP_ID = os.environ.get("etsy_shop_id")
+OAUTH_TOKEN = os.environ.get("etsy_oauth_token")
+OAUTH_TOKEN_SECRET = os.environ.get("etsy_oauth_token_secret")
 
-permission_scopes = ['transactions_r']
+permission_scopes = ["transactions_r"]
+
+# Etsy has their own internal map of country codes that doesn't map to anything.
+# Here are some of the ones that we know we have volunteers in so we can save
+# an API call.
+KNOWN_COUNTRY_CODES = {
+    10: "Antarctica",
+    79: "Canada",
+    91: "Germany",
+    93: "Denmark",
+    97: "Egypt",
+    99: "Spain",
+    103: "France",
+    105: "United Kingdom",
+    112: "Greece",
+    122: "India",
+    123: "Ireland",
+    128: "Italy",
+    150: "Mexico",
+    164: "The Netherlands",
+    167: "New Zealand",
+    172: "Philippines",
+    181: "Russia",
+    193: "Sweden",
+    203: "Turkey",
+    209: "United States",
+    211: "Venezuela",
+    219: "Hong Kong",
+}
 
 
 def get_oauth_keys() -> None:
     """A function that should be run from the command line to generate new keys."""
-    login_url, temp_oauth_token_secret = EtsyOAuthHelper.get_request_url_and_token_secret(
-        KEY, SECRET, permission_scopes
-    )
+    (
+        login_url,
+        temp_oauth_token_secret,
+    ) = EtsyOAuthHelper.get_request_url_and_token_secret(KEY, SECRET, permission_scopes)
 
     query = urlparse.urlparse(login_url).query
-    temp_oauth_token = parse_qs(query)['oauth_token'][0]
+    temp_oauth_token = parse_qs(query)["oauth_token"][0]
     print("URL for verification: ", login_url)
     verifier = input("Please visit the URL above and paste the verification key here: ")
 
@@ -38,18 +67,23 @@ def get_oauth_keys() -> None:
 def etsy_recent_sale_callback() -> None:
     receipts = etsy.findAllShopReceipts(
         shop_id=SHOP_ID,
-        min_last_modified=int((datetime.now() - timedelta(seconds=16)).timestamp())
+        min_created=int((datetime.now() - timedelta(seconds=15)).timestamp()),
     )
 
     for receipt in receipts:
-        msg = (
-            f":moneybag: Just sold a whopping {receipt.get('grandtotal')}"
-            f" {receipt.get('currency_code')} to somebody in {receipt.get('city')}!"
-        )
-        if buyer_msg := receipt.get('message_from_buyer'):
-            msg += f" They included a message for us:\n\n```\n{buyer_msg}\n```"
-        app.client.chat_postMessage(
-            text=msg,
-            channel=rooms_list["merch"],
-            as_user=True
-        )
+        try:
+            country_name = KNOWN_COUNTRY_CODES.get(int(receipt.get("currency_code")))
+            if not country_name:
+                country_name = etsy.getCountry(country_id=receipt.get("currency_code"))[0].get(
+                    "name"
+                )
+            msg = (
+                f":moneybag: Just sold a whopping {receipt.get('grandtotal')}"
+                f" {receipt.get('currency_code')} to somebody in {country_name}!"
+            )
+            if buyer_msg := receipt.get("message_from_buyer"):
+                msg += f" They included a message for us:\n\n```\n{buyer_msg}\n```"
+        except Exception as e:
+            msg = f"Somebody just bought something, but there was an error on my" \
+                  f" side:\n\n```\n{e}\n```"
+        app.client.chat_postMessage(text=msg, channel=rooms_list["merch"], as_user=True)
