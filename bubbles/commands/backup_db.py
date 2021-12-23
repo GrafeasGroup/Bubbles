5553076a7bcef3eb57225f147639d529c93ea1ad
Joe Kaufeld <joe.kaufeld@gmail.com>
2021-12-22 21:20:47 -0500

    add env command

--- bubbles/commands/backup_db.py
+++ bubbles/commands/backup_db.py
@@ -13,26 +13,20 @@ def backup_db(payload):
 
     # the db info is injected into the bot environment, so we'll grab it
     # and regurgitate it for the command
-    env_copy = os.environ.copy()
-    env_copy["PGPASSWORD"] = os.environ.get("postgres_password")
-
+    password = os.environ.get("postgres_password")
     user = os.environ.get("postgres_user")
     db = os.environ.get("postgres_db")
     host = os.environ.get("postgres_host")
     filename = f"db_backup_{str(datetime.now().date())}.tar"
 
-    try:
-        say("Starting DB export. This may take a moment.")
-        subprocess.check_call(
-            shlex.split(
-                f"pg_dump -U {user} {db} -h {host} > {filename}"
-            ), env=env_copy
+    say("Starting DB export. This may take a moment.")
+    subprocess.check_call(
+        shlex.split(
+            f"env PGPASSWORD={password} pg_dump -U {user} {db} -h {host} > {filename}"
         )
-        say("DB export complete. Uploading...")
-        utils.upload_file(file=filename, title=filename)
-    except subprocess.CalledProcessError:
-        say(f"Something went wrong and I'm not sure what. Please review:\n\n")
-        return
+    )
+    say("DB export complete. Uploading...")
+    utils.upload_file(file=filename, title=filename)
 
     p = Path(".")
     previous_backups = list(p.glob("db_backup_*.tar"))
