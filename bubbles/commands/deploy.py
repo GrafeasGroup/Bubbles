e52100745c9e8c49a53fcb4884bdd9f8f814bdeb
Joe Kaufeld <joe.kaufeld@gmail.com>
2022-01-20 09:43:54 -0500

    fix help comment

--- bubbles/commands/deploy.py
+++ bubbles/commands/deploy.py
@@ -137,5 +137,5 @@ def deploy(payload):
 PluginManager.register_plugin(
     deploy,
     r"deploy ?(.+)",
-    help=f"!deploy [{', '.join(SERVICES)}] - deploys the code currently on github to the staging server.",
+    help=f"!deploy [{', '.join(SERVICES)}] - deploys the code currently on github to production.",
 )
