0b7e7f106b683fbfbf9967eac6cf0091de356a8b
David Alexander <opensource@thelonelyghost.com>
2022-01-27 12:59:21 -0500

    Better test support for !wish GitHub integration

--- bubbles/plugins/wishlist.py
+++ bubbles/plugins/wishlist.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 from bubbles.plugins import BaseCommand
 from bubbles.slack import SlackUtils
@@ -6,31 +7,36 @@ from bubbles.slack import SlackUtils
 import requests
 
 
-# On the project page, click on the 3 dots for the column and copy column link.
-# It should look something like this:
-#
-#     https://github.com/orgs/GrafeasGroup/projects/8#column-17559696
-#
-# We extract the ID from there
-github_project_column_id = "17559696"
-github_token = os.environ['GITHUB_TOKEN']
+# This is a class purely because it's easier to override in tests
+class GitHub:
 
+    # On the project page, click on the 3 dots for the column and copy
+    # column link. It should look something like this:
+    #
+    #     https://github.com/orgs/GrafeasGroup/projects/8#column-17559696
+    #
+    # We extract the ID from there
+    project_column_id: str = os.getenv('GITHUB_PROJECT_COLUMN_ID', '17559696')
+    token: Optional[str] = os.getenv('GITHUB_TOKEN')
 
-def new_project_note(suggestion: str) -> None:
-    url = f"https://api.github.com/projects/columns/{github_project_column_id}/cards"
-    headers = {
-        "Authorization": f"bearer {github_token}",
+    def new_project_note(self, suggestion: str) -> None:
+        url = f"https://api.github.com/projects/columns/{self.project_column_id}/cards"
+        if not self.token:
+            raise ValueError('No GitHub token provided. Set it in the environment variable GITHUB_TOKEN')
 
-        # required for GitHub REST API versioning
-        "Content-Type": "application/vnd.github.v3+json",
-    }
-    payload = {
-        "note": suggestion,
-    }
-    response = requests.post(url=url, headers=headers, json=payload)
-    if response.status_code != 201:
-        # GitHub specifically replies with "201 Created" if successful
-        response.raise_for_status()
+        headers = {
+            "Authorization": f"bearer {self.token}",
+
+            # required for GitHub REST API versioning
+            "Content-Type": "application/vnd.github.v3+json",
+        }
+        payload = {
+            "note": suggestion,
+        }
+        response = requests.post(url=url, headers=headers, json=payload)
+        if response.status_code != 201:
+            # GitHub specifically replies with "201 Created" if successful
+            response.raise_for_status()
 
 
 class WishlistCommand(BaseCommand):
@@ -38,5 +44,5 @@ class WishlistCommand(BaseCommand):
     help_text = f'wish [suggestion] - adds a note to the dev suggestion box'
 
     def process(self, msg: str, utils: SlackUtils) -> None:
-        new_project_note(msg)
+        GitHub().new_project_note(msg)
         utils.respond('Your suggestion has been catalogued. Thanks for the input!')
--- test/plugins/test_wishlist.py
+++ test/plugins/test_wishlist.py
@@ -1,12 +1,16 @@
 import pytest
 
+import os
+import warnings
 from unittest.mock import MagicMock, patch
 
 import bubbles.plugins.wishlist as base
 
 
-@patch('bubbles.plugins.wishlist.new_project_note')
-def test_responds_to_trigger_word(new_project_note, slack_utils):
+@patch('bubbles.plugins.wishlist.GitHub')
+def test_responds_to_trigger_word(mock_github, slack_utils):
+    mock_gh_instance = MagicMock()
+    mock_github.return_value = mock_gh_instance
     teleporter = {'msg': ''}
     def track_message(msg):
         teleporter['msg'] = msg
@@ -17,39 +21,63 @@ def test_responds_to_trigger_word(new_project_note, slack_utils):
     assert cmd.is_relevant({'text': '@Bubbles wish people do stupid stuff, people win stupid prizes'}, slack_utils)
     cmd.process('do stupid stuff, win stupid prizes', slack_utils)
 
-    new_project_note.assert_called_once()
+    mock_github.assert_called_once()
+    mock_gh_instance.new_project_note.assert_called_once()
     slack_utils.respond.assert_called_once()
     assert 'suggestion has been catalogued' in teleporter['msg']
 
 
 @patch('bubbles.plugins.wishlist.requests.post')
-@patch('bubbles.plugins.wishlist.github_token')
-def test_post_to_github(github_token, requests_post):
+def test_post_to_github(requests_post):
+    gh = base.GitHub()
     response = MagicMock()
     response.status_code = 201
-    response.raise_for_status.side_effect = Exception('Forbidden!')
     requests_post.return_value = response
 
-    github_token.return_value = 'FakeToken'
-
-    base.new_project_note('Test suggestion, please ignore')
+    gh.token = 'FakeRealToken'
+    gh.new_project_note('Test suggestion, please ignore')
 
     requests_post.assert_called_once()
-    assert not response.raise_for_status.called
+    response.raise_for_status.assert_not_called()
+
+
+@pytest.mark.skipif(not (os.getenv('GITHUB_TOKEN') and os.getenv('EXTERNAL_TESTS')), reason="External end-to-end tests for posting project notes to GitHub require real credentials and manual cleanup, so... screw that for all but exceptional purposes")
+def test_actual_post_to_github():
+    gh = base.GitHub()
+    gh.new_project_note('Test note from automated test suite, please ignore')
+    warnings.warn("Project note created requiring manual cleanup: "
+                  f"https://github.com/orgs/GrafeasGroup/projects/8#column-{gh.project_column_id}")
 
 
 @patch('bubbles.plugins.wishlist.requests.post')
-@patch('bubbles.plugins.wishlist.github_token')
-def test_failed_post_to_github(github_token, requests_post):
+def test_failed_post_to_github(requests_post):
+    gh = base.GitHub()
     response = MagicMock()
     response.status_code = 403
-    response.raise_for_status.side_effect = Exception('Forbidden!')
+    response.raise_for_status.side_effect = PermissionError('Forbidden!')
     requests_post.return_value = response
 
-    github_token.return_value = 'FakeToken'
+    gh.token = 'FakeBadToken'
 
-    with pytest.raises(Exception):
-        base.new_project_note('Test suggestion, please ignore! Should not actually be posted to GitHub anyway...')
+    with pytest.raises(PermissionError):
+        gh.new_project_note('Test suggestion, please ignore! Should not actually be posted to GitHub anyway...')
 
     requests_post.assert_called_once()
     response.raise_for_status.assert_called_once()
+
+
+@patch('bubbles.plugins.wishlist.requests.post')
+def test_missing_github_creds(requests_post):
+    gh = base.GitHub()
+    response = MagicMock()
+    response.status_code = 403
+    response.raise_for_status.side_effect = PermissionError('Forbidden!')
+    requests_post.return_value = response
+
+    gh.token = None
+
+    with pytest.raises(ValueError):
+        gh.new_project_note('Test suggestion, please ignore! Should not actually be posted to GitHub anyway...')
+
+    requests_post.assert_not_called()
+    response.raise_for_status.assert_not_called()
