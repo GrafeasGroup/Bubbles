1777fac83be0e1a1bbba16361d4e05147787f970
David Alexander <opensource@thelonelyghost.com>
2022-01-27 21:09:08 -0500

    Remove stuff that subverts automated tests when imported

--- bubbles/__init__.py
+++
@@ -1,4 +0,0 @@
-# Don't touch!
-# This actually enables loading all the files in the commands subdirectory and
-# works alongside bubbles.commands.__init__
-from bubbles.commands import *
--- bubbles/config.py
+++ bubbles/config.py
@@ -1,97 +1,15 @@
-import argparse
 import os
 from datetime import datetime
-from typing import Any, Dict, List
-from unittest import mock
-import logging
-from unittest.mock import MagicMock
+# from typing import Any, Dict, List
 
-import matplotlib as mpl  # type: ignore
-import praw
-import slack_bolt
-from blossom_wrapper import BlossomAPI  # type: ignore
-from dotenv import load_dotenv
-from etsy2 import Etsy
-from etsy2.oauth import EtsyOAuthClient
-from praw import Reddit  # type: ignore
-from slack_bolt import App  # type: ignore
+import matplotlib as mpl
 
-from bubbles.plugins import PluginManager as PM
+USERNAME = os.getenv("username", "bubbles")
+API_KEY = os.getenv("api_key", None)
+PAYMENT_KEY = os.getenv("payment_key", None)
+PAYMENT_VALUE = os.getenv("payment_value", None)
 
-parser = argparse.ArgumentParser(description="BubblesV2! The very chatty chatbot.")
-parser.add_argument("--startup-check", action="store_true")
-parser.add_argument("--interactive", action="store_true")
-args = parser.parse_args()
-CHECK_MODE = args.startup_check
-INTERACTIVE_MODE = args.interactive
-
-log = logging.getLogger(__name__)
-load_dotenv()
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(funcName)s | %(message)s",
-)
-
-USERNAME = os.environ.get("username", "bubbles")
-API_KEY = os.environ.get("api_key", None)
-DEFAULT_CHANNEL = os.environ.get("default_channel", "dev_test")
-PAYMENT_KEY = os.environ.get("payment_key", None)
-PAYMENT_VALUE = os.environ.get("payment_value", None)
-REDDIT_SECRET = os.environ.get("reddit_secret", None)
-REDDIT_CLIENT_ID = os.environ.get("reddit_client_id", None)
-REDDIT_USER_AGENT = os.environ.get("reddit_user_agent", None)
-
-ENABLE_BLOSSOM = os.environ.get("enable_blossom", False)
-
-try:
-    reddit = Reddit(
-        username=os.environ.get("reddit_username") or "",
-        password=os.environ.get("reddit_password") or "",
-        client_id=os.environ.get("reddit_client_id") or "",
-        client_secret=os.environ.get("reddit_secret") or "",
-        user_agent=os.environ.get("reddit_user_agent") or "",
-    )
-except praw.exceptions.MissingRequiredAttributeException as e:  # type: ignore
-    log.warning(f"Missing required Reddit secret:{e}\nDisabling Reddit access.")
-    reddit = MagicMock()
-
-try:
-    app = App(
-        signing_secret=os.environ.get("slack_signing_secret"),
-        token=os.environ.get("slack_oauth_token"),
-    )
-except slack_bolt.error.BoltError as e:  # type: ignore
-    log.warning(
-        f"Missing required Slack secret: {e}\nDisabling Slack. If you are not running"
-        f" in interactive mode, the bot will not function as expected."
-    )
-    app = MagicMock()
-
-    class AuthResponse:
-        data = {"user_id": "1234"}
-
-    app.client.auth_test.return_value = AuthResponse
-    app.client.users_list.return_value = {
-        "members": [{"real_name": "console", "deleted": False, "id": "abc"}]
-    }
-    app.client.conversations_list.return_value = {
-        "channels": [{"id": "456", "name": DEFAULT_CHANNEL}]
-    }
-
-if ENABLE_BLOSSOM:
-    # Feature flag means that we can lock away blossom functionality until
-    # blossom is actually ready to roll.
-    blossom = BlossomAPI(
-        email=os.getenv("blossom_email") or "",
-        password=os.getenv("blossom_password") or "",
-        api_key=os.getenv("blossom_api_key") or "",
-        api_base_url=os.getenv("blossom_api_url") or "",
-    )
-    print("blossom loaded!")
-else:
-    blossom = mock.MagicMock()
-
+"""
 # There is an overloaded __get__ in the underlying Bolt app, so this type
 # doesn't resolve cleanly.
 ME: str = app.client.auth_test().data["user_id"]  # type: ignore
@@ -130,25 +48,8 @@ DEFAULT_CHANNEL = rooms_list[DEFAULT_CHANNEL]
 mods_array: List = []
 for i in range(0, 24):
     mods_array.append(None)
-
-# Import PluginManager from here
-PluginManager = PM(COMMAND_PREFIXES, INTERACTIVE_MODE)
+"""
 
 mpl.rcParams["figure.figsize"] = [20, 10]
-
-try:
-    etsy = Etsy(
-        etsy_oauth_client=EtsyOAuthClient(
-            client_key=os.environ.get("etsy_key"),
-            client_secret=os.environ.get("etsy_secret"),
-            resource_owner_key=os.environ.get("etsy_oauth_token"),
-            resource_owner_secret=os.environ.get("etsy_oauth_token_secret"),
-        )
-    )
-except ValueError:
-    # Like everything Etsy does, this library is half-assed too
-    log.warning("Missing one or more required Etsy secrets. Disabling Etsy.")
-    etsy = MagicMock()
-
 
 TIME_STARTED = datetime.now()
