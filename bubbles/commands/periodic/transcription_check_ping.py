81e79836a0ab93ddd7ae20dbdd3d7a291d4fc9ad
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-03-09 20:25:06 +0100

    Aggregate check reminders first by time then by mod

--- bubbles/commands/periodic/transcription_check_ping.py
+++ bubbles/commands/periodic/transcription_check_ping.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime, timedelta
 import re
 from enum import Enum
@@ -9,7 +10,6 @@ from bubbles.commands.periodic import (
 )
 from bubbles.config import app, users_list, rooms_list
 from bubbles.commands.helper_functions_history.extract_author import extract_author
-import urllib
 
 
 USERNAME_REGEX = re.compile("u/(?P<username>[^ *:]+)")
@@ -23,9 +23,13 @@ RESOLVED_REACTIONS = [
 ]
 PENDING_REACTIONS = ["watch", "speech_bubble", "speech_balloon"]
 
+# Determines after which time a check should give a reminder
+CHECK_SEARCH_START_DELTA = timedelta(hours=12)
+# Determines how far back the bot searches for checks
+CHECK_SEARCH_END_DELTA = timedelta(days=10)
+
 # The times to group the checks by
 CHECK_TIMES = [
-    (timedelta(hours=12), "<12 hours"),
     (timedelta(days=2), "12-48 hours"),
     (timedelta(days=4), "2-4 days"),
     (timedelta(days=7), "4-7 days"),
@@ -192,7 +196,7 @@ def _aggregate_checks_by_mod(checks: List[CheckData]) -> Dict:
     """Aggregate the given checks by the handling moderator."""
     aggregate = {
         "unclaimed": [],
-        "mods": {},
+        "claimed": {},
     }
 
     for check in checks:
@@ -200,9 +204,9 @@ def _aggregate_checks_by_mod(checks: List[CheckData]) -> Dict:
             aggregate["unclaimed"].append(check)
         else:
             mod = check["mod"]
-            mod_checks = aggregate["mods"].get(mod, [])
+            mod_checks = aggregate["claimed"].get(mod, [])
             mod_checks.append(check)
-            aggregate["mods"][mod] = mod_checks
+            aggregate["claimed"][mod] = mod_checks
 
     return aggregate
 
@@ -218,7 +222,7 @@ def _aggregate_checks_by_time(checks: List[CheckData]) -> List:
     index = 0
 
     # Aggregate the checks by their time
-    for delta, title in CHECK_TIMES:
+    for delta, time_str in CHECK_TIMES:
         time_checks = []
 
         if index >= len(checks):
@@ -239,156 +243,70 @@ def _aggregate_checks_by_time(checks: List[CheckData]) -> List:
         if len(time_checks) > 0:
             # Further aggregate by moderators
             mod_aggregate = _aggregate_checks_by_mod(time_checks)
-            aggregate.append((title, mod_aggregate))
+            aggregate.append((time_str, mod_aggregate))
 
     return aggregate
 
 
+def _get_check_fragment(check: CheckData) -> str:
+    """Get the reminder text fragment for a single check."""
+    user = check.get("user") or "[UNKNOWN]"
+    link = check["link"] or None
+
+    return f"<{link}|{user}" if link else f"{user} (LINK NOT FOUND)"
+
+
+def _get_check_reminder(aggregate: List) -> str:
+    """Get the reminder text for the checks."""
+    reminder = "*Pending Transcription Checks:*\n\n"
+
+    for time_str, mod_aggregate in aggregate:
+        reminder += f"*{time_str}*:\n"
+
+        # Add unclaimed checks
+        unclaimed = mod_aggregate["unclaimed"]
+        if len(unclaimed) > 0:
+            reminder += "- *[UNCLAIMED]*: "
+            fragments = [_get_check_fragment(check) for check in unclaimed]
+            reminder += ",".join(fragments) + "\n"
+
+        # Add claimed checks
+        for mod, claimed in mod_aggregate["claimed"].items():
+            reminder += f"- *{mod}*: "
+            fragments = [_get_check_fragment(check) for check in claimed]
+            reminder += ",".join(fragments) + "\n"
+
+    return reminder
+
+
 def transcription_check_ping_callback() -> None:
+    now = datetime.now()
 
-    tic = time.time()
-
-    timestamp_needed_end_cry = datetime.datetime.now() - datetime.timedelta(days=8)
-    timestamp_needed_start_cry = datetime.datetime.now() - datetime.timedelta(hours=12)
-    timestamp_needed_start_cry_harder = datetime.datetime.now() - datetime.timedelta(
-        days=7
-    )
+    start_time = now - CHECK_SEARCH_START_DELTA
+    end_time = now - CHECK_SEARCH_END_DELTA
 
     response = app.client.conversations_history(
         channel=rooms_list[TRANSCRIPTION_CHECK_CHANNEL],
-        oldest=timestamp_needed_end_cry.timestamp(),
-        latest=timestamp_needed_start_cry.timestamp(),
-        limit=1000,
+        oldest=end_time,
+        latest=start_time,
+        limit=200,
     )
-    cry = False
-    cry_harder = False
-    users_to_welcome = {}
-    mod_having_reacted = {}
-    mod_having_reacted_harder = {}
-    i = 0
-    last_text = ""
-    for message in response["messages"]:
-        last_text = message["text"]
-        if (
-            message["user"] != users_list["Blossom"]
-        ):  # Do not handle messages not from Blossom
-            continue
-        author = extract_author(message, GOOD_REACTIONS)
-        if author == "Nobody":
-            cry = True
-            try:
-                username, permalink = get_username_and_permalink(message)
-                mod_reacting = extract_author(
-                    message, ["watch", "speech_bubble", "speech_balloon"]
-                )
-            except IndexError:
-                # This is a message that didn't come from Kierra and isn't something we
-                # can process. Ignore it and move onto the next message.
-                continue
-            except urllib.error.URLError as e:
-                # It should definitely not reach this part, but usually posts are checked quickly. It
-                # will reach this part if the rate limit has been reached.
-                time.sleep(60)
-                app.client.chat_postMessage(
-                    channel=rooms_list[TRANSCRIPTION_CHECK_PING_CHANNEL],
-                    link_names=1,
-                    text="ERROR! RATE LIMIT REACHED! I must stop here :(",
-                    unfurl_links=False,
-                    unfurl_media=False,
-                    as_user=True,
-                )
-                break
-            if (
-                datetime.datetime.fromtimestamp(float(message["ts"]))
-                >= timestamp_needed_start_cry_harder
-            ):
-                users_to_welcome[i] = (username, permalink)
-                if mod_reacting not in mod_having_reacted.keys():
-                    mod_having_reacted[mod_reacting] = []
-                mod_having_reacted[mod_reacting].append(users_to_welcome[i])
-                i = i + 1
-            else:
-                cry_harder = True
-                if mod_reacting not in mod_having_reacted_harder.keys():
-                    mod_having_reacted_harder[mod_reacting] = []
-                mod_having_reacted_harder[mod_reacting].append((username, permalink))
+    if not response.get("ok"):
+        logging.error("Failed to get check messages!")
+        return
 
-    if cry:
-        for mod in mod_having_reacted.keys():
-            i = 0
-            page = 0
-            if mod == "Nobody":
-                text = "for [_NOBODY CLAIMED US :(_]: "
-            elif mod == "Conflict":
-                text = "for [_TOO MANY PEOPLE CLAIMED US :(_]: "
-            else:
-                text = "for *" + mod + "*: "
-            for data in mod_having_reacted[mod]:
-                username, permalink = data
-                text = text + "<" + str(permalink) + "|" + str(username) + ">, "
-                if i == 10:
-                    if page == 0:
-                        text = "List of unchecked transcriptions " + text + "..."
-                    else:
-                        text = "(page " + str(page + 1) + ") " + text + "..."
-                    app.client.chat_postMessage(
-                        channel=rooms_list[TRANSCRIPTION_CHECK_PING_CHANNEL],
-                        link_names=1,
-                        text=text,
-                        unfurl_links=False,
-                        unfurl_media=False,
-                        as_user=True,
-                    )
-                    text = ""
-                    page = page + 1
-                    i = -1
-                i = i + 1
-            text = text[:-2]
-            if page == 0:
-                text = "List of unchecked transcriptions " + text
-            else:
-                text = "(last page) " + text
-            app.client.chat_postMessage(
-                channel=rooms_list[TRANSCRIPTION_CHECK_PING_CHANNEL],
-                link_names=1,
-                text=text,
-                unfurl_links=False,
-                unfurl_media=False,
-                as_user=True,
-            )
-    if cry_harder:
-        for mod in mod_having_reacted_harder.keys():
-            if mod == "Nobody":
-                text = "[_NOBODY CLAIMED US :(_]: "
-            elif mod == "Conflict":
-                text = "[_TOO MANY PEOPLE CLAIMED US :(_]: "
-            else:
-                text = "*" + mod + "* "
-            text = (
-                ":rotating_light: :pinged: :rotating_light: "
-                + text
-                + " These users have not fixed their transcriptions in a whole week. Please check the reactions to this message and, if correct, log these volunteers on the 'Volunteer Warning' spreadsheet: "
-            )
-            for data in mod_having_reacted_harder[mod]:
-                username, permalink = data
-                text = text + "<" + str(permalink) + "|" + str(username) + ">, "
-            text = text[:-2]
-            app.client.chat_postMessage(
-                channel=rooms_list[TRANSCRIPTION_CHECK_PING_CHANNEL],
-                link_names=1,
-                text=text,
-                unfurl_links=False,
-                unfurl_media=False,
-                as_user=True,
-            )
-        # print("CALLBACK ENDED! Time elapsed: "+str(time.time()-tic)+" s")
-        # print("Last message:" +str(last_text))
+    # Get the reminder for the checks
+    messages = response["messages"]
+    checks = _extract_open_checks(messages)
+    aggregate = _aggregate_checks_by_time(checks)
+    reminder = _get_check_reminder(aggregate)
 
-
-#    else:
-#        response = client.chat_postMessage(
-#            channel=DEFAULT_CHANNEL,
-#            text="All users have been welcomed. Good.",
-#            as_user=True,
-#        )
-#    print("Trigger time:" + str(datetime.datetime.now()))
+    # Post the reminder in Slack
+    app.client.chat_postMessage(
+        channel=rooms_list[TRANSCRIPTION_CHECK_PING_CHANNEL],
+        link_names=1,
+        text=reminder,
+        unfurl_links=False,
+        unfurl_media=False,
+        as_user=True,
+    )
