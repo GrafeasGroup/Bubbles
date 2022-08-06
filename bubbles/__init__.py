48f8644f69508b2dd4eef21bbc9fb0f31e0f5a36
Joe Kaufeld <opensource@joekaufeld.com>
2022-08-06 01:01:53 -0400

    remove dead code

--- bubbles/__init__.py
+++ bubbles/__init__.py
@@ -1,5 +1,1 @@
-# Don't touch!
-# This actually enables loading all the files in the commands subdirectory and
-# works alongside bubbles.commands.__init__
-# from bubbles.commands import *
 __version__ = "?????"  # This is populated by the CI pipeline
