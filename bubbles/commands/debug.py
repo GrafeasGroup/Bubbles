7eb9847c261719ed6750c8fc7254a73f6b3d3a93
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-08-21 21:15:43 +0200

    Add rule_monitoring output to debug command

--- bubbles/commands/debug.py
+++ bubbles/commands/debug.py
@@ -2,6 +2,7 @@ from bubbles.commands import Plugin
 from bubbles.commands.periodic.transcription_check_ping import (
     transcription_check_ping_callback,
 )
+from bubbles.commands.periodic.rule_monitoring import new_subreddits, subreddit_stack
 
 
 def debug(payload: dict) -> None:
@@ -11,6 +12,12 @@ def debug(payload: dict) -> None:
     if "transcription_check_ping" in text:
         say("Manually triggering check pings.")
         transcription_check_ping_callback()
+    elif "rule_monitoring" in text:
+        new_subs = ", ".join(new_subreddits)
+        sub_stack = ", ".join(subreddit_stack)
+        say(f"New subreddits:\n{new_subs}\n\nSubreddit stack:{sub_stack}")
+    else:
+        say("Not sure what you want to debug.")
 
 
 PLUGIN = Plugin(callable=debug, regex=r"^debug")
