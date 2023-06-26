0c0d9fbe0b8775c2af3d1363fd36e6e2504e5b00
Tim Jentzsch <commits@timjen.net>
2023-06-26 19:54:47 +0200

    Fix accessing user token in checkping command

--- bubbles/commands/check_ping.py
+++ bubbles/commands/check_ping.py
@@ -7,9 +7,7 @@ from bubbles.utils import parse_user
 def check_ping(payload: Payload) -> None:
     """!check_ping [user] - Manually trigger a transcription check ping."""
     tokens = payload.cleaned_text.split()
-
-    user_raw = tokens.get(1)
-    user_filter = parse_user(user_raw) if user_raw is not None else None
+    user_filter = parse_user(tokens[1]) if len(tokens) > 1 else None
 
     transcription_check_ping(payload.get_channel(), user_filter=user_filter, start_now=True)
 
